9e2aaa9a1f9da2be84a01b8a8687d2bf89a956e1
LeungMingching <43911379+LeungMingching@users.noreply.github.com>
2020-09-13 20:24:19 +0800

    Add files via upload

---
+++ image1_programing.m
@@ -0,0 +1,32 @@
+clear all;
+clc;
+
+%Display the image
+
+%read the original text file to a 64*64 matrix
+fileID = fopen('charact1.txt');
+im1_ori = fscanf(fileID,'%s');
+im1_ori = reshape(im1_ori, [64,64]);
+fclose(fileID);
+
+%convert each element from 32 base to dec
+for j = 1:64
+    for i = 1:64
+        im1_ori(i,j) = base2dec(im1_ori(i,j),32);
+    end
+end
+
+%convert char matrix to num matrix
+im1_ori = double(im1_ori);
+
+%display the image
+
+imshow(im1_ori);
+
+%thresholding
+im1_thr= zeros(64);
+for j = 1:64
+    for i = 1:64
+        if 
+    end
+end
